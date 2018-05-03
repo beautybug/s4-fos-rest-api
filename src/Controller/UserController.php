8dfc2081e3d4f084c1d423111b4bdf5d19cc0e79
tangyue <tangyue@howzhi.com>
2018-05-03 14:26:34 +0800

    新增service

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,16 +5,20 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use FOS\RestBundle\Controller\Annotations;
+use Psr\Log\LoggerInterface;
+use App\Service\MessageGenerator;
 
 class UserController extends BaseController
 {
     /**
      * @Annotations\Get(path="/user")
      */
-    public function getAction()
+    public function getAction(MessageGenerator $messageGenerator)
     {
+        $message = $messageGenerator->getHappyMessage();
+        $user = $this->container->get('user.userService')->getUser();
         return new JsonResponse(
-            'User'
+            $user
         );
     }
 }
---
+++ src/Service/User/UserService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Service\User;
+
+class UserService
+{
+    public function getUser()
+    {
+        return [
+            'id' => 2,
+            'name' => 'tangyue',
+            'gender' => 'female',
+        ];
+    }
+}
